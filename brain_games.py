0936763dcae85f880ad80e36e1fbf361069f840d
Диана Захарчук <heilig1996@mail.ru>
2024-12-05 20:14:16 +0300

    update brain_games/scripts/brain_games.py

--- brain_games.py
+++
@@ -1,3 +0,0 @@
-Welcome to the Brain Games!
-if __name__ == '__main__':
-    main()
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,1 +1,6 @@
-Welcome to the Brain Games!
+def greet():
+    print(__name__)
+    __name__ = 'Welcome to the Brain Games!'
+
+    if __name__ == '__main__':
+       return main()
