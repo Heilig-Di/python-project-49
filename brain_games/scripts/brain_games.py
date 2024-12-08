0cc6a141f9afb155cfea599babbb77885aa90639
Диана Захарчук <heilig1996@mail.ru>
2024-12-08 15:39:41 +0300

    restore brain_games/scripts/brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,10 @@
-def greet():
+def greet(__name__):
     print(__name__)
     __name__ = 'Welcome to the Brain Games!'
 
     if __name__ == '__main__':
-       return main()
+       main()
+
+import prompt
+name = prompt.string('May I have your name? ')
+print('Hello, ' + name'!')
