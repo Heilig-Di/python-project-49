965caf3ef9acfcb10708df4b49de9f74ed625613
Диана Захарчук <heilig1996@mail.ru>
2024-12-16 02:45:15 +0300

    add brain_games/scripts/__pycache__/brain_even.cpython-312.pyc

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,31 +1,41 @@
 #!/usr/bin/env python3
-
-from brain_games.cli import welcome_user
+import prompt
 import random
 
-print('Answer "yes" if the number is even, otherwise answer "no".')
-correct = 0
 
-while correct < 3:
-    number = random.randint(1, 100)
+def welcome_user():
+    name = prompt.string('May I have your name? ')
+    print('Hello, ', name, '!')
+    return name
 
-    def check_even(number):
+def main():
+    print('Welcome to the Brain Games!')
 
-        if number % 2 == 0:
-            return 'even'
+
+    name = welcome_user()
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    correct = 0
+
+    while correct < 3:
+        number = random.randint(1, 100)
+
+        def check_even(number):
+
+            if number % 2 == 0:
+                return 'even'
+
+            else:
+                return 'not even'
+
+        result = check_even(number)
+
+        answer = input(f'Question: {number}\nYour answer: ').lower()
+        if (answer == 'yes' and result == 'even') or (answer == 'no' and result == 'not even'):
+            print('Correct!')
+            correct += 1
 
         else:
-            return 'not even'
+            print(f"'{answer}' is wrong answer :(\nLet's try again, {name}!")
+            correct = 0
 
-    result = check_even(number)
-
-    answer = input(f'Question: {number}\nYour answer: ').lower()
-    if (answer == 'yes' and result == 'even') or (answer == 'no' and result == 'not even'):
-        print('Correct!')
-        correct += 1
-
-    else:
-        print(f"'{answer}' is wrong answer :(\nLet's try again, {name}!")
-        correct = 0
-
-print(f'Congratulations, {name}!')
+    print(f'Congratulations, {name}!')
