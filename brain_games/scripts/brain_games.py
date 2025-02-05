f1740951b254527a7403b971f8b18882ce52491c
Диана <heilig1996@mail.ru>
2025-02-05 14:29:04 +0300

    add brain_games/scripts/brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,14 +2,11 @@ from brain_games.cli import welcome_user
 
 
 def greet():
-	__name__ = 'Welcome to the Brain Games!'
-	print(__name__)
-
-	if __name__ == '__main__':
-		return main()
+	print('Welcome to the Brain Games!')
 
 
 def main():
+	greet()
 	welcome_user()
 
 
