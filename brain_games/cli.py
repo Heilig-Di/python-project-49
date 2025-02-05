1f4acb5eae2987686f757cafc002471e626096f8
Диана <heilig1996@mail.ru>
2025-02-05 17:35:10 +0300

    add brain_games/__pycache__/cli.cpython-312.pyc

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -13,5 +13,5 @@ if __name__ == '__main__':
 
 def welcome_user():
     name = prompt.string('May I have your name? ')
-    print('Hello, ', name, '!')
+    print(f'Hello, {name}!')
     return name
