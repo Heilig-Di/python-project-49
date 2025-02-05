a56c0ed5ec0a1a23f8021db5e44be9aad093c96a
Диана <heilig1996@mail.ru>
2025-02-05 17:57:19 +0300

    add brain_games/scripts/brain_gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -7,21 +7,20 @@ from brain_games.games.game_logic import play
 def main():
 	message = 'Find the greatest common divisor of given numbers.'
 
-	def find_divisor(num):
-		divisors = []
-		for i in range(1, int(num / 2) + 1):
-			if num % i == 0:
-				divisors.append(i)
-		return divisors
+	def find_divisor(num_one, num_two):
+		while num_one != 0 and num_two != 0:
+			if num_one > num_two:
+				num_one = num_one % num_two
+			else:
+				num_two = num_two % num_one
+		return num_one + num_two
 
 	def run_game():
 		num_one = random.randint(1, 100)
 		num_two = random.randint(1, 100)
 
 		question = f'{num_one} {num_two}'
-		divisor_num_one = find_divisor(num_one)
-		divisor_num_two = find_divisor(num_two)
-		result = max([num for num in divisor_num_one if num in divisor_num_two])
+		result = find_divisor(num_one, num_two)
 		correct_answer = str(result)
 
 		return question, correct_answer 
