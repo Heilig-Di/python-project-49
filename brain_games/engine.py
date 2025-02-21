1e15a4d35bf4e1f913f68f9dbff3c92420d9c931
Диана <heilig1996@mail.ru>
2025-02-21 17:30:15 +0300

    add brain_games/engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,24 +1,28 @@
 import prompt
 
 from brain_games.cli import welcome_user
-from brain_games.consts import (CONGRATULTIONS_MESSAGE, CORRECT_MESSAGE,
-				ROUNDS, WRONG_MESSAGE)
+from brain_games.consts import (
+    CONGRATULTIONS_MESSAGE,
+    CORRECT_MESSAGE,
+    ROUNDS,
+    WRONG_MESSAGE,
+)
 
 
 def give_results(generate_round, INSTRUCTION):
 
-	name = welcome_user()
-	print(INSTRUCTION)
+    name = welcome_user()
+    print(INSTRUCTION)
 
-	for _ in range(ROUNDS):
-		question, correct_answer = generate_round()
-		print(f'Question: {question}')
-		answer = prompt.string('Your answer: ').lower()
+    for _ in range(ROUNDS):
+        question, correct_answer = generate_round()
+        print(f"Question: {question}")
+        answer = prompt.string("Your answer: ").lower()
 
-		if answer == correct_answer:
-			print(CORRECT_MESSAGE)
+        if answer == correct_answer:
+            print(CORRECT_MESSAGE)
 
-		else:
-			print(WRONG_MESSAGE.format(answer, name))
-			return
-	print(CONGRATULTIONS_MESSAGE.format(name))
+        else:
+            print(WRONG_MESSAGE.format(answer, name))
+            return
+    print(CONGRATULTIONS_MESSAGE.format(name))
